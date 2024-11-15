963a764d3a5c6853c9fe7a0634bdc596d8c4ca51
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-15 20:21:13 +0200

    finished

--- CalcPi.java
+++ CalcPi.java
@@ -13,8 +13,8 @@ public class CalcPi {
 			}
 			denominator = denominator + 2;
 		}
-		System.out.println("Pi according to Java:  "+Math.PI);
-		System.out.println("pi, approximated:      "+sum  * 4);
+		System.out.println("Pi according to Java: "+Math.PI);
+		System.out.println("Pi, approximated:     "+sum  * 4);
 
 	}
 }
