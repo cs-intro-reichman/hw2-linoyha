6735b4e1d4d69146a76d0bf5bd4443d55d3f7f70
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-15 16:31:25 +0200

    finished

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,28 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String cheers= args[0];
+            int spell = Integer.parseInt(args[1]);
+            String letter = "AEFHILMNORSX"; 
+            cheers = cheers.toUpperCase();
+            int i=0;
+            while ( i < cheers.length()) {
+                char c = cheers.charAt(i);
+                if(letter.indexOf(c) != -1) {
+                System.out.println("Give me an "+c +": " +c +"!");
+                }
+                else {
+                System.out.println("Give me a "+c +": " +c +"!");
+                }
+                i++;
+            }
+            System.out.println("What does that spell?");
+            
+            for (int k =0; k<spell; k++) {
+                System.out.println(cheers+"!!!");
+            }
+
+
+             
         }
 }
