1385db2b46d6883b82ae65592ea6b7a8f4c29fae
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-15 17:15:33 +0200

    finished

--- CalcPi.java
+++ CalcPi.java
@@ -2,6 +2,13 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    int num = Integer.parseInt(args[0]);
-		
+		int denominator = 3;
+		double sum =1;
+		for (int i=0; i<num; i++) {
+			sum =
+
+
+		}
+
 	}
 }
