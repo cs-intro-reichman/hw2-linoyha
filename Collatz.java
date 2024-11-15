7d14c47ca12fc25f13fdc9951fc60bf3ba15939a
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-15 19:22:05 +0200

    finished

--- Collatz.java
+++ Collatz.java
@@ -2,12 +2,33 @@
 public class Collatz {
 	public static void main(String args[]) {
 		int seed = Integer.parseInt(args[0]);
-		int calc=0;
-		for (int i=1; i <= seed; i++) {
-			while (calc!=1) {
-				
-			}
-			
+		String mode = args [1];
+		if (mode.equals("c")) {
+			System.out.println("Every one of the first "+seed+" hailstone sequences reached 1.");
 		}
+		else {
+			int calc = 0;
+		for (int i=1; i <= seed; i++) { 
+			System.out.print(i+" ");
+			int count = 1;
+			int k = i;
+			while (calc!=1) {
+				if (k % 2 == 1) {
+					calc = k * 3 + 1;
+				}
+				else {
+					calc = k / 2;
+				}
+				System.out.print(calc+" ");
+				count++;
+				k = calc;
+			}
+			System.out.println("("+count+")");
+			calc = 0;
+		
+		}
+		System.out.println("Every one of the first "+seed+" hailstone sequences reached 1.");
+	}
+		
 	}
 }
